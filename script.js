228aa79c834ecb64a70fec6d417a4f7e96255d49
Mhmd.S <mhmdslmn20@gmail.com>
2022-04-15 03:36:59 +0800

    - Add the third function and passed all tests

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ function reverseString(word) {
     return word.join('')
 }
 
+const calculator = {
+    add(num1, num2) {
+        return num1 + num2
+    },
+    subtract(num1, num2) {
+        return num1 - num2
+    },
+    divide(num1, num2) {
+        return num1 / num2
+    },
+    multiply(num1, num2) {
+        return num1 * num2
+    }
+}
 
 
-
-export {capitalize, reverseString}
+export {capitalize, reverseString, calculator}
--- script.test.js
+++ script.test.js
@@ -1,10 +1,14 @@
-import {capitalize, reverseString} from './script.js'
+import {capitalize, reverseString, calculator} from './script.js'
 
-describe('script.js test', () => {
+describe('script.js first function test', () => {
     
     test('Capitalize first letter of the word', () => {
         expect(capitalize('hello')).toBe('Hello')
     })
+
+});
+
+describe('Second function test', () => {
 
     test('Reverse a string [123]', () => {
         expect(reverseString('123')).toBe('321')
@@ -14,4 +18,24 @@ describe('script.js test', () => {
         expect(reverseString('ABCabc!@??')).toBe('??@!cbaCBA')
     })
 
-})
+});
+
+describe('Third function test', () => {
+
+    test('Calculator multiply function', () => {
+        expect(calculator.multiply(1, 2)).toBe(2)
+    })
+    
+    test('Calculator add function', () => {
+        expect(calculator.add(1, 2)).toBe(3)
+    })
+
+    test('Calculator subtract function', () => {
+        expect(calculator.subtract(1, 2)).toBe(-1)
+    })
+
+    test('Calculator divide function', () => {
+        expect(calculator.divide(1, 2)).toBe(1/2)
+    })
+
+});
